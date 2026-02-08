c03f74307b29dfbd15cd5e67b8e0fe435d0b86af
Madjiguene Niang <madjixx94@gmail.com>
2026-02-08 15:43:40 +0100

    Updated about.component.css

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -7,6 +7,7 @@ interface Service {
   titleKey: string;
   descKey: string;
   image: string;
+  color: string;
 }
 
 @Component({
@@ -24,25 +25,29 @@ export class ServicesComponent {
       id: 1,
       titleKey: 'service1.title',
       descKey: 'service1.desc',
-      image: 'assets/entreprise_et_bureau.jpg'
+      image: 'assets/entreprise_et_bureau.jpg',
+      color: '#1DA1F2'
     },
     {
       id: 2,
       titleKey: 'service2.title',
       descKey: 'service2.desc',
-      image: 'assets/commerce_&_etablissement.jpg'
+      image: 'assets/commerce_&_etablissement.jpg',
+      color: '#2E8B57'
     },
     {
       id: 3,
       titleKey: 'service3.title',
       descKey: 'service3.desc',
-      image: 'https://images.pexels.com/photos/5691622/pexels-photo-5691622.jpeg?auto=compress&cs=tinysrgb&w=800'
+      image: 'https://images.pexels.com/photos/5691622/pexels-photo-5691622.jpeg?auto=compress&cs=tinysrgb&w=800',
+      color: '#1DA1F2'
     },
     {
       id: 4,
       titleKey: 'service4.title',
       descKey: 'service4.desc',
-      image: 'https://images.pexels.com/photos/1571460/pexels-photo-1571460.jpeg?auto=compress&cs=tinysrgb&w=800'
+      image: 'https://images.pexels.com/photos/1571460/pexels-photo-1571460.jpeg?auto=compress&cs=tinysrgb&w=800',
+      color: '#2E8B57'
     }
   ];
 }
--- src/app/components/values/values.component.ts
+++ src/app/components/values/values.component.ts
@@ -6,6 +6,7 @@ interface Value {
   id: number;
   titleKey: string;
   descKey: string;
+  color: string;
 }
 
 @Component({
@@ -22,22 +23,26 @@ export class ValuesComponent {
     {
       id: 1,
       titleKey: 'value1.title',
-      descKey: 'value1.desc'
+      descKey: 'value1.desc',
+      color: '#1DA1F2'
     },
     {
       id: 2,
       titleKey: 'value2.title',
-      descKey: 'value2.desc'
+      descKey: 'value2.desc',
+      color: '#2E8B57'
     },
     {
       id: 3,
       titleKey: 'value3.title',
-      descKey: 'value3.desc'
+      descKey: 'value3.desc',
+      color: '#1DA1F2'
     },
     {
       id: 4,
       titleKey: 'value4.title',
-      descKey: 'value4.desc'
+      descKey: 'value4.desc',
+      color: '#2E8B57'
     }
   ];
 }
