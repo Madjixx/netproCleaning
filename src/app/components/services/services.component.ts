359fc60590c444b613b20164aa6cb84c86863982
Madjiguene Niang <madjixx94@gmail.com>
2026-02-08 16:11:00 +0100

    Updated header.component.html

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -25,28 +25,28 @@ export class ServicesComponent {
       id: 1,
       titleKey: 'service1.title',
       descKey: 'service1.desc',
-      image: '/assets/entreprise_et_bureau.jpg',
+      image: 'assets/entreprise_et_bureau.jpg',
       color: '#1DA1F2'
     },
     {
       id: 2,
       titleKey: 'service2.title',
       descKey: 'service2.desc',
-      image: '/assets/commerce_and_etablissement.jpg',
+      image: 'assets/commerce_and_etablissement.jpg',
       color: '#2E8B57'
     },
     {
       id: 3,
       titleKey: 'service3.title',
       descKey: 'service3.desc',
-      image: '/assets/fin-chantier.jpeg',
+      image: 'assets/fin-chantier.jpeg',
       color: '#1DA1F2'
     },
     {
       id: 4,
       titleKey: 'service4.title',
       descKey: 'service4.desc',
-      image: '/assets/residence.jpeg',
+      image: 'assets/residence.jpeg',
       color: '#2E8B57'
     }
   ];
